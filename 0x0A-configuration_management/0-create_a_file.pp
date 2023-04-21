79c411813413c88236eb53c783fdbc01e0e0d5b6
Tayo Akinrinsoye <tayoakinrinsoye@hotmail.com>
2023-04-21 18:02:06 +0100

    Task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 # Create a file named school in /tmp
-
-file {'/tmp/school':
-  ensure  => 'file'
+file { '/tmp/school':
+  ensure  => 'file',
   content => 'I love Puppet',
   group   => 'www-data',
   mode    => '0744',
-  owner   => 'www-data'
+  owner   => 'www-data',
 }
